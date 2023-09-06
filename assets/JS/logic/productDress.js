4548cce85670f4c7c2313ade18c6abaad5ed03a7
Nguyen Cong Thanh <thanhproe2k2@gmail.com>
2023-09-07 00:30:15 +0700

    upload productDress and productShirt API

---
+++ assets/JS/logic/productDress.js
@@ -0,0 +1,31 @@
+import productApi from "../api/productAPI";
+import { renderProductList } from "../utils/productRender";
+import { renderPagination } from "../utils/renderPagination";
+
+const itemsPerPage = 3; // Số mục trên mỗi trang
+
+// Hàm để lấy và hiển thị dữ liệu từ trang cụ thể
+async function getDataForPage(page) {
+  try {
+    const startIndex = (page - 1) * itemsPerPage; // Tính toán vị trí bắt đầu
+
+    const { arrayProduct, pagination } = await productApi.getProductsDress(
+      page,
+      itemsPerPage
+    );
+    renderProductList("product__List", arrayProduct);
+
+    // Hiển thị thông tin phân trang
+    renderPagination("pagination", page, pagination.totalPages, onPageClick);
+  } catch (error) {
+    console.error("Lỗi khi lấy dữ liệu sản phẩm: ", error);
+  }
+}
+
+// Hàm xử lý sự kiện khi người dùng nhấp vào một trang cụ thể
+function onPageClick(page) {
+  getDataForPage(page);
+}
+
+// Khởi đầu: Lấy dữ liệu cho trang đầu tiên
+getDataForPage(1);
